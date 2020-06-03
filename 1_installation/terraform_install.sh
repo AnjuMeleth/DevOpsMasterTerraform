7d49eb1fe0661e59cb25603cde02d4c28e3a745c
Anju <anjurose.d@gmail.com>
2020-06-03 05:10:54 +0000

    Added task 6

--- 1_installation/terraform_install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install unzip
-wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
-unzip terraform_0.12.26_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
---
+++ 6_tag/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "my-instance" {
+  ami                    = "ami-0b44050b2d893d5f7"
+  instance_type          = "t2.micro"
+
+tags = {
+    Name = "ProdServer1"
+  }
+}
