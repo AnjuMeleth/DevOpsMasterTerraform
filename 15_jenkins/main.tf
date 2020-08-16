07eb8182eb4362bf08d136d532ae3e0f3c62744e
anju <anjurose.d@gmail.com>
2020-08-16 11:18:01 +0000

    Added txt files

--- 15_jenkins/main.tf
+++ 15_jenkins/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
   ami                    = "ami-0b44050b2d893d5f7"
   instance_type          = "t2.micro"
-  key_name 		 = "JenkinsMumbai"
+  key_name 		 = "anjutest"
   vpc_security_group_ids = [aws_security_group.secGrp1.id] 
 user_data = <<-EOF
 		#! /bin/bash
--- 6_tag/main.tf
+++ 6_tag/main.tf
@@ -4,8 +4,19 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
   ami                    = "ami-0b44050b2d893d5f7"
   instance_type          = "t2.micro"
-
+  key_name 		 = "anjutest"
+  vpc_security_group_ids = [aws_security_group.secGrp1.id] 
 tags = {
     Name = "ProdServer1"
   }
 }
+resource "aws_security_group" "secGrp1" {
+
+  ingress {
+    from_port   = 22 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
