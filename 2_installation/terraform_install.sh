7036db243b37c4e905b1d27c221a8a19425b59b1
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2020-08-16 12:09:30 +0530

    Update terraform_install.sh

--- 2_installation/terraform_install.sh
+++ 2_installation/terraform_install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install unzip
-wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.13.0_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.13.0/terraform_0.13.0_linux_amd64.zip
 unzip terraform_0.13.0_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
